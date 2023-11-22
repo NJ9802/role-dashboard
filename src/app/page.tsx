7f2a99bf30c0ff55f02b11419cd328d063c9258a
NJ9802 <nelsonjavier9802@gmail.com>
2023-11-22 16:59:41 -0500

    Update config for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import RoleDashboard from "@/components/RoleDashboard/RoleDashboard";
 import { prisma } from "@/db/prismaClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const roles = await prisma.role.findMany();
   const permissions = await prisma.permissions.findFirst();
